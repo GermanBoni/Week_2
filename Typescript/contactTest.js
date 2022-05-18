58df2d8dadb1b3160e249ae4949dbfcdcb1b3267
GermanBoni <german_villarreal_@hotmail.com>
2022-05-18 17:43:42 +0200

    eliminacion

--- Typescript/contactTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var person_1 = require("./person");
-var contacts_1 = require("./contacts");
-var contacto = new contacts_1.Contacts();
-var persona1 = new person_1.Person("Ramon", 23, "Valencia");
-var persona2 = new person_1.Person("Jaime", 40, "Castellón");
-contacto.people = [persona1, persona2];
-contacto.printCalendar();
